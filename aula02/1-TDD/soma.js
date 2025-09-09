3befe7d4fccb104d577f4599b7bb2671782e6f4f
AlessandraRosaRezende <alessandra.rosarezende@gmail.com>
2025-09-09 14:29:15 -0300

    feat: código inicial

---
+++ aula02/1-TDD/soma.js
@@ -0,0 +1,5 @@
+// Declaração da função 'soma' que recebe dois parâmetros: 'num1' e 'num2'
+// A função está vazia, então não realiza nenhuma operação e retorna 'undefined' por padrão
+const soma = (num1, num2) => {
+
+}
---
+++ aula02/2-Atividade/login.js
@@ -0,0 +1,8 @@
+function login(username, password) {
+  const correctUsername = 'coderUser';
+  const correctPassword = '123';
+
+  
+}
+
+module.exports = login;
